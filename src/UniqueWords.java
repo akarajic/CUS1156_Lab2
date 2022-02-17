e1aa5fc87d0f050df9a99b929d12a624efbab4fa
Adna <adna.karajic19@my.stjohns.edu>
2022-02-16 21:20:25 -0500

    First commit

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -7,16 +7,18 @@ public class UniqueWords
 		@param list ArrayList of strings to be examined
 		@return number of unique strings in the list
    */
-   public static int countUnique(ArrayList<String> list)
-   {
-	  int count = 0;
-	  
-      for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
-      }
+	public static int countUnique(ArrayList<String> list) {
+		int count = 0;
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = 0; j < list.size(); j++) {
+				if (list.get(i).equals(list.get(j)) && j != i) {
+					break;
+				} else if (j == list.size() - 1) {
+					count++;
+				}
+
+			}
+		}
 	  return count;
    }
 
